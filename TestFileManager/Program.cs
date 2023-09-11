9e97277a3b313f02c1e0bcaed6dbdcbf31faa0a4
Stanislav <rotarstanislav4@gmail.com>
2023-09-11 10:06:47 +0300

    Add functionality for testing library

--- TestFileManager/Program.cs
+++ TestFileManager/Program.cs
@@ -8,39 +8,43 @@ class Program
         fileManager.RegisterXmlFormat();
         fileManager.RegisterJsonFormat();
 
-        string filePath = "data.json"; 
-        string format = "json"; 
-/*
+        string jsonFilePath = "data.json",xmlFilePath = "data.xml"; 
+        string jsonFormat = "json",xmlFormat="xml"; 
+
         List<Record> records = new List<Record>();
 
         records.Add(new Record { Date = DateTime.Now, BrandName = "New Car 1", Price = 50000 });
         records.Add(new Record { Date = DateTime.Now, BrandName = "New Car 2", Price = 60000 });
 
-        //fileManager.WriteFile(filePath, format, records);
+        fileManager.WriteFile(jsonFilePath, jsonFormat, records);
+        fileManager.WriteFile(xmlFilePath, xmlFormat, records);
 
         Console.WriteLine("Records saved successfully.");
 
-        var newRecordXml = new Record { Date = DateTime.Now, BrandName = "New Car 3", Price = 1000 };
-        fileManager.AddRecordToFile(filePath, format, newRecordXml);
+        var newRecordXml = new Record { Date = DateTime.Now, BrandName = "New Car Xml", Price = 1000 };
+        fileManager.AddRecordToFile(xmlFilePath, xmlFormat, newRecordXml);
+        
+        var newRecordJson = new Record { Date = DateTime.Now, BrandName = "New Car Json", Price = 1000 };
+        fileManager.AddRecordToFile(jsonFilePath, jsonFormat, newRecordXml);
 
         Console.WriteLine("Record add successfully.");
 
-        List<Record> updatedRecords = fileManager.ReadFile(filePath, format);
-        Console.WriteLine("Updated Records:");
+        List<Record> updatedRecords = fileManager.ReadFile(xmlFilePath, xmlFormat);
+        Console.WriteLine("Updated Records:Xml");
         foreach (var record in updatedRecords)
         {
             Console.WriteLine($"Date: {record.Date}, BrandName: {record.BrandName}, Price: {record.Price}");
         }
         
-        fileManager.DeleteRecord(filePath,format,"New Car 3");
+        fileManager.DeleteRecord(jsonFilePath,jsonFormat,"New Car 2");
         
-        updatedRecords = fileManager.ReadFile(filePath, format);
-        Console.WriteLine("Updated Records:");
+        updatedRecords = fileManager.ReadFile(jsonFilePath, jsonFormat);
+        Console.WriteLine("Updated Records:Json");
         foreach (var record in updatedRecords)
         {
             Console.WriteLine($"Date: {record.Date}, BrandName: {record.BrandName}, Price: {record.Price}");
         }
-        */
-        fileManager.ConvertFile(filePath,format,"lolik.xml","xml");
+        
+        fileManager.ConvertFile(xmlFilePath,xmlFormat,"test.json","json");
     }
 }
